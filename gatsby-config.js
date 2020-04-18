04fd5f56a0de5a03955ba638896c98fbaeb8abd7
victor atencio <victorj.atencio@gmail.com>
2020-04-18 18:16:14 -0400

    Trubleshooting

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       options: {
         typeName: `WPGraphQL`,
         fieldName: `wpgraphql`,
-        url: `http://agrupacionvirgendelujan/graphql`,
+        url: `http://wp.agrupacionvirgendelujan.com/graphql`,
       },
     },
   ],
