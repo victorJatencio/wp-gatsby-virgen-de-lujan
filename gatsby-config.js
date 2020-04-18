6925c2f5e62ddaaa28ad7e9218e419f999920db4
victor atencio <victorj.atencio@gmail.com>
2020-04-18 15:17:23 -0400

    New commit new

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         typeName: `WPGraphQL`,
         fieldName: `wpgraphql`,
         url: `http://agrupacionvirgendelujan/graphql`,
-        refetchInterval: 60,
+        // refetchInterval: 60,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
