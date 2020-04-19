c34c53e7605d09df52290e90ff3bc00e131f1b1f
victor atencio <victorj.atencio@gmail.com>
2020-04-19 07:38:58 -0400

    deploy test

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-source-graphql`,
-      options: {
-        typeName: `WPGraphQL`,
-        fieldName: `wpgraphql`,
-        url: `http://agrupacionvirgendelujan/graphql`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-graphql`,
+    //   options: {
+    //     typeName: `WPGraphQL`,
+    //     fieldName: `wpgraphql`,
+    //     url: `http://agrupacionvirgendelujan/graphql`,
+    //   },
+    // },
   ],
 }
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,26 +1,26 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
+// import React from "react"
+// import { graphql } from "gatsby"
+// import Layout from "../components/layout"
 
-export const query = graphql`
-  query($id: ID!) {
-    wpgraphql {
-      post(id: $id) {
-        title
-        content
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query($id: ID!) {
+//     wpgraphql {
+//       post(id: $id) {
+//         title
+//         content
+//       }
+//     }
+//   }
+// `
 
-const PostTemplate = ({ data }) => {
-  const post = data.wpgraphql.post
-  return (
-    <Layout>
-      <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
-    </Layout>
-  )
-}
+// const PostTemplate = ({ data }) => {
+//   const post = data.wpgraphql.post
+//   return (
+//     <Layout>
+//       <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
+//       <div dangerouslySetInnerHTML={{ __html: post.content }} />
+//     </Layout>
+//   )
+// }
 
-export default PostTemplate
+// export default PostTemplate
